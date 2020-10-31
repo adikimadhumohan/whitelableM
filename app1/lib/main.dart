a575e81ea36e81a4f42f922a3ba963b1f2170986
madhu <madhumohan.a@ecoihub.com>
2020-10-31 18:34:50 +0530

    mycom

--- app1/lib/main.dart
+++ app1/lib/main.dart
@@ -9,10 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-//hi from my end
-
-  //hi from server
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
